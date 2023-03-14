975729be3ea865c9d5e4d37e13029790cc380289
nicklassundin <p4cr@hotmail.com>
2023-03-14 15:23:55 +0100

    initial save

---
+++ modules/test-setup.spec.js
@@ -0,0 +1,14 @@
+const sinon = require("sinon");
+const chai = require("chai");
+
+beforeEach(function () {
+
+    this.sandbox = sinon.createSandbox();
+
+});
+
+afterEach(function () {
+
+    this.sandbox.restore();
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+// Const $ = require("jquery");
+const TerserPlugin = require("terser-webpack-plugin");
+//const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const webpack = require("webpack");
+const dir = __dirname;
+module.exports = [
+    {
+        "entry": {
+            "bundle": "./lib.js"
+        },
+        "module": {
+            "rules": [
+                {
+                    "loader": "raw-loader",
+                    "test": /\.txt$/u
+                },
+                {
+                    "loader": "csv-loader",
+                    "test": /\.csv$/u
+
+                    /*
+                     * Options: {
+                     * Header: true,
+                     * Download: true,
+                     * SkipEmptyLines: true
+                     * }
+                     */
+                }
+            ]
+        },
+        "node": {
+            /*
+            "child_process": "empty",
+            "console": true,
+            "fs": "empty",
+            "net": "empty",
+            "tls": "empty"
+
+             */
+        },
+        "resolve": {
+          "fallback": {
+              "http":  require.resolve("stream-http"),
+              "url": require.resolve("url/"),
+              "https": require.resolve("https-browserify"),
+              "fs": false
+          }
+        },
+        "optimization": {
+            "minimize": true,
+            "minimizer": [
+                new TerserPlugin()
+            ]
+        },
+        "output": {
+            "filename": "./bundle.js",
+            "path": `${dir}/client`
+        },
+        "plugins": [
+            /*
+             * New webpack.ProvidePlugin({
+             * $: "jquery",
+             * JQuery: "jquery",
+             * "window.jQuery": "jquery"
+             * })
+             */
+        ]
+    }
+];
+
